fc149b79b4a5c67463f872beedf81cba9036872e
Vladyslav Kruchok <vladyslavw@gmail.com>
2022-10-18 21:32:10 +0300

    fix mentor's remarks

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -7,7 +7,7 @@ const authenticate = async (req, res, next) => {
     try {
         const { authorization = "" } = req.headers;
         const [bearer = "", token = ""] = authorization.split(" ");
-        if (bearer !== "Bearer") {
+        if (bearer !== "Bearer" || !token) {
             throw requestError(401);
         }
         try {
