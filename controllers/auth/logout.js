f8d2c14081b2885924139dadcd8e896c38d7f0e1
Vladyslav Kruchok <vladyslavw@gmail.com>
2023-01-02 23:16:47 +0200

    fix logout control, add .json() to line 6

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,10 +1,9 @@
 const { authModel } = require("../../models");
 
-const logout = async (req, res) => { 
+const logout = async (req, res) => {
     const { _id } = req.user;
     await authModel.User.findByIdAndUpdate(_id, { token: "" });
-
-    res.status(204);
+    res.status(204).json();
 };
 
 module.exports = logout;
